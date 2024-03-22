82860f13d98f104ab47170c30210a16df17751ca
Craig <craig.mwangi@student.moringaschool.com>
2024-03-22 20:17:10 +0300

    Labs

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,36 @@
 require ( './helpers.js' );
 
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+  cats.push(name);
+}
+function destructivelyPrependCat(name){
+cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+  cats.pop();
+}
+function appendCat(name){
+  const newCats = [...cats, name];
+  return newCats;
+}
+function prependCat(name){
+  const newCats = [name, ...cats];
+  return newCats;
+}
+function removeLastCat(){
+  const newCats = cats.slice(0, -1);
+  return newCats;
+}
+function removeFirstCat(){
+  const newCats = cats.slice(1);
+  return newCats;
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+}
+
 describe('index.js', function () {
   describe('cats', function () {
     it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
